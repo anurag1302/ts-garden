67798f6c778ebfcfe6bc78dc335007e64b9b91d5
Anurag <anurag3487@gmail.com>
2025-03-23 14:51:00 +0530

    ts type aliases

--- dist/app.js
+++ dist/app.js
@@ -2,4 +2,8 @@
 const func = (a, b) => {
     return a * b;
 };
+const func2 = (a, b) => {
+    return a + b;
+};
 console.log(func(10, 20));
+console.log(func2(10, 30));
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+type numbers = (a: number, b: number) => number; //type aliases
+
 const func = (a: number, b: number): number => {
   return a * b;
 };
 
+const func2: numbers = (a, b) => {
+  return a + b;
+};
+
 console.log(func(10, 20));
+console.log(func2(10, 30));
