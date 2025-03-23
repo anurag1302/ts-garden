fb46b1b6c7474c930d5a8c545f839876c7270031
Anurag <anurag3487@gmail.com>
2025-03-23 15:01:25 +0530

    ts arrays

--- dist/app.js
+++ dist/app.js
@@ -5,5 +5,20 @@ const func = (a, b) => {
 const func2 = (a, b) => {
     return a + b;
 };
+//Arrays
+const arr1 = ["a", "b", "c"];
+const arr2 = [1, 2, 3];
+const arr3 = ["A", 10, "B", 20];
+console.log("*****************ARRAYS******************************");
+arr1.forEach((item) => {
+    console.log(item.toLocaleUpperCase());
+});
+arr2.forEach((item) => {
+    console.log(item * 2);
+});
+arr3.forEach((item) => {
+    console.log(item);
+});
+console.log("*****************FUNCTIONS******************************");
 console.log(func(10, 20));
 console.log(func2(10, 30));
--- src/app.ts
+++ src/app.ts
@@ -8,5 +8,26 @@ const func2: numbers = (a, b) => {
   return a + b;
 };
 
+//Arrays
+
+const arr1: string[] = ["a", "b", "c"];
+const arr2: Array<number> = [1, 2, 3];
+const arr3: Array<string | number> = ["A", 10, "B", 20];
+
+console.log("*****************ARRAYS******************************");
+
+arr1.forEach((item) => {
+  console.log(item.toLocaleUpperCase());
+});
+
+arr2.forEach((item) => {
+  console.log(item * 2);
+});
+
+arr3.forEach((item) => {
+  console.log(item);
+});
+
+console.log("*****************FUNCTIONS******************************");
 console.log(func(10, 20));
 console.log(func2(10, 30));
